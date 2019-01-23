d087e67dc2722139c2e76275640878ed9d34e728
Ian Richard <ianrichard03@gmail.com>
2019-01-22 23:20:55 -0600

    added post route for registering users.

--- controller/controller.js
+++ controller/controller.js
@@ -1,13 +1,31 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
+const db = require("../models");
 mongoose.connect('mongodb://localhost:27017/vueliche');
 
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/users";
-mongoose.connect(MONGODB_URI);
+mongoose.connect(MONGODB_URI, {useNewUrlParser: true});
 
-router.get("/", ( request, response) => {
-  response.send("200 OK");
+router.get("/users", ( request, response) => {
+    db.users.find({}).then((data)=>{
+        response.send(data);
+    }).catch((error)=>{
+        response.json(error);
+    });
+});
+
+router.post("/register", ( request, response) => {
+    db.users.create({
+        username: request.body.username,
+        email: request.body.email,
+        password: request.body.password,
+        authorization: request.body.authorization
+    }).then(function() {
+        response.send("User Added.");
+    }).catch(function(error){
+        response.json(error);
+    });
 });
 
 module.exports = router;
