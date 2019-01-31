da6b81e8abf7a77957fb067cc43f28504f9addc2
Ian Richard <ianrichard03@gmail.com>
2019-01-30 19:03:28 -0600

    replace things at random.

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
-const controller = require("./controller/controller")
+const controller = require("./controller/controller");
+var serveStatic = require('serve-static');
 
 
 // Define middleware here
@@ -12,7 +13,9 @@ app.use(express.json());
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  //app.use(express.static("client/build"));
+  app.use(serveStatic(__dirname + "/dist"));
+
 }
 
 app.use(express.static(path.join(__dirname,"./public")));
