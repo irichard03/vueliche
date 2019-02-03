6bd27ef314ad39c1bbcd46517e7eb35c14d9bd48
Ian Richard <ianrichard03@gmail.com>
2019-02-03 01:24:23 -0600

    added sign in and auth.

--- controller/controller.js
+++ controller/controller.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+const salt = bcrypt.genSaltSync(10);
+const jwt = require("jsonwebtoken");
+const config = require("./config");
 const mongoose = require('mongoose');
 const db = require("../models");
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/users";
@@ -23,18 +27,34 @@ router.post("/checkuser", ( request, response) => {
 });
 
 router.post("/register", ( request, response) => {
+    let token = jwt.sign({id: request.body.email}, config.secret, {expiresIn: 259200});
+    let user = request.body.email;
     db.users.create({
-        username: request.body.username,
         email: request.body.email,
-        password: request.body.password,
+        password: bcrypt.hashSync(request.body.password, salt),
         authorization: request.body.authorization
     }).then(function() {
-        response.send("User Added.");
+        response.send({ auth: true, token: token, user: user});
     }).catch(function(error){
         response.json(error);
     });
 });
 
-
-
+router.post("/login", ( request, response) => {
+    let user = request.body.email;
+    db.users.findOne({ email: request.body.email}).then((data) => {
+        if(!data) {
+            response.send("Email address not found, please try again or register.");
+            let verifiedPassword = bcrypt.compareSync(request.body.password, data.password);
+            if(!verifiedPassword) {
+                response.send("Email address not found, please try again or register.");
+                let token = jwt.sign({id: request.body.email}, config.secret, {expiresIn: 259200});
+                response.send(({ auth: true, token: token, user: user}));
+            }   
+        }
+    }).catch((error) => {
+        response.json(error);
+    });
+});
+    
 module.exports = router;
